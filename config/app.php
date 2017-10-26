5702218affbe58a878ab6ea93f75421a2bce3215
pelwd <15010420@anabuki-college.ac.jp>
2017-10-26 14:30:47 +0900

    Revert "あど"
    This reverts commit d757c88daa35bcd89f158d0992cc7379de6d477d.

--- config/app.php
+++ config/app.php
@@ -177,7 +177,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Barryvdh\Cors\ServiceProvider::class,
-        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
     ],
 
@@ -227,8 +226,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
-        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
 
     ],
 
