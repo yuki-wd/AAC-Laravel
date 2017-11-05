e962bb68cf9ab689c2d2c793b7da436ecd14e7ba
pelwd <15010420@anabuki-college.ac.jp>
2017-11-05 11:33:39 +0900

    Add score

--- database/migrations/2017_10_11_082645_create_questions_table.php
+++ database/migrations/2017_10_11_082645_create_questions_table.php
@@ -16,6 +16,7 @@ class CreateQuestionsTable extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->increments('id');
             $table->string('content');
+            $table->integer('score');
             $table->integer('test_id');
             $table->timestamps();
         });
